63d968363699c789885d4b2bfc6d618daafcd0c7
DeePZeeRa <dasdeepayan08@gmail.com>
2025-08-15 19:20:34 +0530

    fix: relative path for context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/common/Navbar"
-import { UserProvider } from "@/utils/useContext";
+import { UserProvider } from "./[context]/UserContext";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
